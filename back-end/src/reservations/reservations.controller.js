7d3d31090e1582b7f4faeabc7e2fc4ed8eee1da6
Lilith Vox <lilyvluce@outlook.com>
2021-07-10 13:08:22 -0700

    date query working

--- back-end/src/reservations/reservations.controller.js
+++ back-end/src/reservations/reservations.controller.js
@@ -5,17 +5,20 @@ const asyncErrorHandler = require('../errors/asyncErrorHandler');
  * @returns a list of reservations to the client
  */
 async function list(req, res) {
-  const data = await service.list();
-  res.json(data);
+  if (req.query?.date) listByDate(req, res);
+  else {
+    const data = await service.list();
+    res.json(data);
+  }
 }
 /**
  * List handler for a specific date
+ * is called in @list() if a query is found
  */
 async function listByDate(req, res) {
   const theDate = req.query.date;
-  console.trace('date: ' + theDate);
+  console.info('date: ' + theDate);
   const data = await service.listByDate(theDate);
-  console.trace('service returns: ' + data);
   res.status(200).json(data);
 }
 
