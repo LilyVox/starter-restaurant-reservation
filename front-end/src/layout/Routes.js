61d2926c5c0e4cc9be6ef158d2becffe3b89ccb6
Lilith Vox <lilyvluce@outlook.com>
2021-07-11 11:13:26 -0700

    added back Routes file

---
+++ front-end/src/layout/Routes.js
@@ -0,0 +1,33 @@
+import React from "react";
+
+import { Redirect, Route, Switch } from "react-router-dom";
+import Dashboard from "../dashboard/Dashboard";
+import NotFound from "./subComponents/NotFound";
+import { today } from "../utils/date-time";
+
+/**
+ * Defines all the routes for the application.
+ *
+ *
+ * @returns {JSX.Element}
+ */
+function Routes() {
+  return (
+    <Switch>
+      <Route exact={true} path="/">
+        <Redirect to={"/dashboard"} />
+      </Route>
+      <Route exact={true} path="/reservations">
+        <Redirect to={"/dashboard"} />
+      </Route>
+      <Route path="/dashboard">
+        <Dashboard date={today()} />
+      </Route>
+      <Route>
+        <NotFound />
+      </Route>
+    </Switch>
+  );
+}
+
+export default Routes;
