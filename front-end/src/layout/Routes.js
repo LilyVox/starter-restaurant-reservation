c55f278480223271475f82aa7489e928ca7a2c7c
Lilith Vox <lilyvluce@outlook.com>
2021-07-17 15:43:16 -0700

    added capturing routes for /reservations/

--- front-end/src/layout/Routes.js
+++ front-end/src/layout/Routes.js
@@ -1,11 +1,11 @@
-import React from "react";
+import React from 'react';
 
-import { Redirect, Route, Switch } from "react-router-dom";
-import Dashboard from "../dashboard/Dashboard";
-import NotFound from "./subComponents/NotFound";
+import { Redirect, Route, Switch } from 'react-router-dom';
+import Dashboard from '../dashboard/Dashboard';
+import NotFound from './subComponents/NotFound';
 import ReservationPage from '../reservations/main';
 import TableMain from '../tables/main';
-import { today } from "../utils/date-time";
+import { today } from '../utils/date-time';
 import useQuery from '../utils/useQuery';
 
 /**
@@ -17,23 +17,26 @@ import useQuery from '../utils/useQuery';
 function Routes() {
   const query = useQuery();
   const date = query.get('date');
-  console.info('routes date from query: ' + date)
+  console.info('routes date from query: ' + date);
   return (
     <Switch>
-      <Route exact={true} path="/">
-        <Redirect to={"/dashboard"} />
+      <Route exact={true} path='/'>
+        <Redirect to={'/dashboard'} />
       </Route>
-      <Route exact={true} path="/reservations">
-        <Redirect to={"/dashboard"} />
-      </Route>
-      <Route exact={true} path="/reservations/new">
+      <Route exact={true} path='/reservations/new'>
         <ReservationPage />
       </Route>
-      <Route exact={true} path="/tables/new">
+      <Route exact={true} path='/reservations/:reservation_id'>
+        <Redirect to={'/dashboard'} />
+      </Route>
+      <Route exact={true} path='/reservations'>
+        <Redirect to={'/dashboard'} />
+      </Route>
+      <Route exact={true} path='/tables/new'>
         <TableMain />
       </Route>
-      <Route path="/dashboard">
-        <Dashboard date={date? date :today()} />
+      <Route path='/dashboard'>
+        <Dashboard date={date ? date : today()} />
       </Route>
       <Route>
         <NotFound />
